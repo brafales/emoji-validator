7bf0f62ea2e7ffd2f8c4d183585d12def59371c1
Mark Burns <markthedeveloper@gmail.com>
2017-12-14 10:30:42 +0000

    ignore nil values

--- lib/emoji/validator/no_emoji_validator.rb
+++ lib/emoji/validator/no_emoji_validator.rb
@@ -14,6 +14,7 @@ module Emoji
     #
     class NoEmojiValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
+        return if value.nil?
         return if value.match(Unicode::Emoji::REGEX_VALID).nil?
         record.errors.add(attribute, :has_emojis)
       end
